c69a3c81cec090ff4641f9ccd1d2110c4b913d1c
Maxime Duhamel <max.duh22@gmail.com>
2024-01-17 07:24:30 +0100

    léger avancement

--- generator.php
+++ generator.php
@@ -1,13 +1,23 @@
 <?php
-$datas=[];
-if (file_exists("datas.json")) {
-  $fic=file("datas.json");
-  $json="";
-  foreach($fic as $line) {
-    $json.=$line;
+  $datas=[];
+  
+  if (file_exists("datas.json")) {
+    $fic=file("datas.json");
+    $json="";
+    foreach($fic as $line) {
+      $json.=$line;
+    }
+    $datas=json_decode($json, true);
+  } else {
+    for($mois=0;$mois<12;$mois++) {
+      for($jour=1;$jour<date('t', mktime(0, 0, 0, $mois+1, 1, date("Y")))+1;$jour++) {
+        $datas[$mois."/".$jour] = [];
+      }
+    }
+    $fic = fopen("datas.json", "w");
+    fwrite($fic, json_encode($datas, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
   }
-  $datas=json_decode($json, true);
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +43,6 @@ if (file_exists("datas.json")) {
   </div>
   <script src="js/script.js"></script>
 </div>
-<!-- partial -->
 
 </body>
 </html>
